e57927f9bc13fbe0df9a0a652c8858780e89b18e
Marwanmhks <66640498+Marwanmhks@users.noreply.github.com>
2022-06-26 14:06:30 +0200

    NODE 2 fixed

---
+++ Node 2-3/Systick.c
@@ -0,0 +1,57 @@
+#include "Systick.h"
+
+
+
+volatile uint32_t tickPer10ms = 0;
+volatile FlagType systickFlag = 0;
+volatile FlagType oneSecondFlag = 0;
+
+/******************************************************************************/
+/* Inputs: None        												  */
+/* Outputs: sets the flags for the tasks of the application        												  */
+/* Re-entrancy: Re-entrant        												  */
+/* Synchronous :									*/
+/* Description: ISR for the systick timer with period 10ms													  */
+/******************************************************************************/
+void SysTickIntHandler(void){
+	systickFlag = 1;
+	tickPer10ms++;
+	if(tickPer10ms == 100){
+		oneSecondFlag = 1;
+		tickPer10ms = 0;
+	}
+}
+/******************************************************************************/
+/* Inputs: None        												  */
+/* Outputs: flag of the systick timer       												  */
+/* Re-entrancy: Re-entrant        												  */
+/* Synchronous :									*/
+/* Description: checks the flag of the systick timer 10 ms periodicity											  */
+/******************************************************************************/
+FlagType systick_elapsed(void){
+	return systickFlag;
+}
+/******************************************************************************/
+/* Inputs: None        												  */
+/* Outputs: flag of the systick timer       												  */
+/* Re-entrancy: Re-entrant        												  */
+/* Synchronous :									*/
+/* Description: checks the flag of the systick timer 1 s periodicity											  */
+/******************************************************************************/
+FlagType oneSec_elapsed(){
+	return oneSecondFlag;
+}
+void oneSec_reset(){
+	oneSecondFlag = 0;
+}
+void systick_reset(){
+	systickFlag = 0;
+}
+
+void Systick_Init(TimeType ECU_period){
+		IntMasterEnable();
+    SysTickPeriodSet(SysCtlClockGet()*ECU_period);
+    SysTickIntRegister(SysTickIntHandler);
+    SysTickIntEnable();
+    SysTickEnable();
+}
--- Node 2-3/app.c
+++ Node 2-3/app.c
@@ -1,75 +1,23 @@
 #include "App.h"
 
 volatile uint8_t currentState = 0;
-volatile uint32_t tickPer10ms = 0;
-volatile FlagType systickFlag = 0;
-volatile FlagType oneSecondFlag = 0;
 
-/******************************************************************************/
-/* Inputs: None        												  */
-/* Outputs: sets the flags for the tasks of the application        												  */
-/* Re-entrancy: Re-entrant        												  */
-/* Synchronous :									*/
-/* Description: ISR for the systick timer with period 10ms													  */
-/******************************************************************************/
-void SysTickIntHandler(void){
-	systickFlag = 1;
-	tickPer10ms++;
-	if(tickPer10ms == 100){
-		oneSecondFlag = 1;
-		tickPer10ms = 0;
-	}
-}
 
-/******************************************************************************/
-/* Inputs: None        												  */
-/* Outputs: flag of the systick timer       												  */
-/* Re-entrancy: Re-entrant        												  */
-/* Synchronous :									*/
-/* Description: checks the flag of the systick timer 10 ms periodicity											  */
-/******************************************************************************/
-FlagType systick_elapsed(){
-	return systickFlag;
-}
-/******************************************************************************/
-/* Inputs: None        												  */
-/* Outputs: flag of the systick timer       												  */
-/* Re-entrancy: Re-entrant        												  */
-/* Synchronous :									*/
-/* Description: checks the flag of the systick timer 1 s periodicity											  */
-/******************************************************************************/
-FlagType oneSec_elapsed(){
-	return oneSecondFlag;
-}
-void oneSec_reset(){
-	oneSecondFlag = 0;
-}
-void systick_reset(){
-	systickFlag = 0;
-}
-void Systick_Init(void){
-		IntMasterEnable();
-    SysTickPeriodSet(SysCtlClockGet()*ECU_period);
-    SysTickIntRegister(SysTickIntHandler);
-    SysTickIntEnable();
-    SysTickEnable();
-}
-
-void Init_LEDs(void) {
-	LEDs;
-	Port_ConfigType LED_config;
-	LED_config.PinMode = Port_PinMode_DIO;
-	LED_config.PinDirection = PORT_PIN_OUT;
-	uint32_t len = (sizeof(DioChannelConfigData)/sizeof(Dio_ChannelType));
-	for(int i=0; i< len; i++){
-		LED_config.Pin = DioChannelConfigData[i];
-		Port_Init(&LED_config);
-	}
-}
-void GPIO_Init(void){
+void Init(void){
 		Init_LEDs();
+		CAN_Init();
+		Systick_Init(ECU_period);
 }
-
+void Loop_Func(void){
+			if(systick_elapsed()){
+				State_Machine();
+				systick_reset(); 
+     }
+			if (oneSec_elapsed()){
+				State_Change(currentState);
+				oneSec_reset();
+			}
+}
 void State_Machine(void){
   switch(CAN_recieve()){
         case INCREMENT:
@@ -86,12 +34,6 @@ void State_Machine(void){
 	}
 }
 
-void State_Change(void){
-	Dio_ChannelType color_States[] = {RED, BLUE, GREEN};
-	Dio_WriteChannel(color_States[0],STD_LOW);
-	Dio_WriteChannel(color_States[1],STD_LOW);
-	Dio_WriteChannel(color_States[2],STD_LOW);
-	Dio_WriteChannel(color_States[currentState],STD_HIGH);
-}
+
 
 
